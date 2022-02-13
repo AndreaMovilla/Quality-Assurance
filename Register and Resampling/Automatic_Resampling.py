7840076ea82f817fffdef56efbe6edfb7d156da3
AndreaMovilla <86127817+AndreaMovilla@users.noreply.github.com>
2022-02-13 19:33:58 +0100

    Importing directories errors fixed

--- Register and Resampling/Automatic_Resampling.py
+++ Register and Resampling/Automatic_Resampling.py
@@ -10,11 +10,15 @@ class Path:
     def __init__(self):
         self.selected_directory = "None"
         self.folder_path = tk.StringVar()
-
+        self.file_path = tk.StringVar()
+        
     def browse_button(self):
         self.selected_directory = filedialog.askdirectory()
         self.folder_path.set(self.selected_directory)
-
+        
+    def browse_file(self):
+        self.selected_file = filedialog.askopenfilename()
+        self.file_path.set(self.selected_file)
 
 analysis = Path()
 reference = Path()
@@ -34,10 +38,13 @@ def mycode():
     root = tk.Tk()
     root.withdraw()
 
-    path_fixed_image = reference.selected_directory  # path to reference registered image
+    path_fixed_image = reference.selected_file  # path to reference registered image
     path_segmentations_folder = analysis.selected_directory  # path to segmentations we want to resample
+    master.destroy()
+    
     paths_segmentations = QA_functions.directories(path_segmentations_folder)
     names_segmentations = QA_functions.names(path_segmentations_folder)
+    
     #  Create output folder and change directories
     d = os.path.dirname('Automatic_Resampling.py')  # directory of script
     p = r'Output_Resampling'.format(d)
@@ -53,17 +60,17 @@ master.title('Automatic Resampling')
 tk.Label(master, text="Reference image").grid(row=0)
 tk.Label(master, text="Segmentation folder").grid(row=1)
 
-e1 = tk.Button(master, text="Select reference image", command=reference.browse_button)
-lbl1 = tk.Label(master, textvariable=reference.folder_path)
-lbl1.grid(row=0, column=2)
+e1 = tk.Button(master, text="Select reference image", command=reference.browse_file)
+lbl1 = tk.Label(master, textvariable=reference.file_path)
 e2 = tk.Button(master, text="Select segmentation folder", command=analysis.browse_button)
 lbl2 = tk.Label(master, textvariable=analysis.folder_path)
-lbl2.grid(row=1, column=2)
 
+lbl1.grid(row=0, column=2)
+lbl2.grid(row=1, column=2)
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 
 
-tk.Button(master, text='Quit', command=master.quit).grid(row=6, column=1, sticky=tk.W, pady=4)
+tk.Button(master, text='Run', command=mycode()).grid(row=6, column=1, sticky=tk.W, pady=4)
 
 tk.mainloop()
